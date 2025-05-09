f34b316e5c9a0ee44a538dd4a2300382e95b3bb2
Addi Trejo <125314660+Additrejo@users.noreply.github.com>
2025-05-08 23:37:50 -0600

    Add files via upload

---
+++ Matlab Onramp/2. Comandos/Denominación de las variables/tarea2_2.m
@@ -0,0 +1,5 @@
+% MATLAB Onramp curso
+% Addi Trejo
+% Tarea: Calcule (a+A)/2. Asigne el resultado a la variable avgAa.
+
+avgAa = (a+A)/2
---
+++ Matlab Onramp/2. Comandos/Denominación de las variables/tarea2_3.m
@@ -0,0 +1,5 @@
+% MATLAB Onramp curso
+% Addi Trejo
+% Tarea: Intente crear la variable 3sq = 9 para demostrar este comportamiento.
+
+3sq = 9
